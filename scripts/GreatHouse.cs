57f51d00def89910cac8efb104c96794f5f9d226
Lucas Maloni <l.maloni.c@gmail.com>
2026-03-27 19:14:20 -0300

    Criação de cena principal com adição de elementos gráficos

--- scripts/GreatHouse.cs
+++ scripts/GreatHouse.cs
@@ -7,7 +7,9 @@ public partial class GreatHouse : Sprite2D
 {
     [Export] 
     public string HouseName { get; set; }
-    public Vector2 targetDimensions { get; set; } = new Vector2(32, 32);
+    public Vector2 targetDimensions { get; set; } = new Vector2(128, 141);
+    // As dimensões do icone são 600x660 - de forma que as dimensões que queremos devem ser (x, 1.1x)
+    // Tem forma melhor de fazer isso ao invés de só declarar cada uma, mas agora não vou priorizar essa otimização mínima
 
     public override void _Ready()
     {
@@ -22,7 +24,6 @@ public partial class GreatHouse : Sprite2D
                 
                 Scale = targetDimensions / textureDimension;
 
-                GD.Print($"escala final: {Scale}");
             }
             else
             {
