c34b63c872da5dd213d2d44602ac36230cfe6a02
lucasmaloni <l.maloni.c@gmail.com>
2026-03-31 13:53:19 -0300

    Desenho de linhas acontecendo. Ta lindo

--- scripts/GraphController.cs
+++ scripts/GraphController.cs
@@ -1,12 +1,14 @@
 using Godot;
 using GCollections = Godot.Collections;
 using System.Collections.Generic;
+using System;
 
 public partial class GraphController : Node2D
 {
 	protected Dictionary<GreatHouse, Dictionary<GreatHouse, Edge>> Graph { get; private set; } = new Dictionary<GreatHouse, Dictionary<GreatHouse, Edge>>();
 	protected Dictionary<string, GreatHouse> HouseLookup { get; private set; } = new Dictionary<string, GreatHouse>();
 	protected DataManager dataManager = new DataManager();
+	protected HashSet<Tuple<GreatHouse, GreatHouse>> drawnConnections = new HashSet<Tuple<GreatHouse, GreatHouse>>();
 
 	public override void _Ready()
 	{
@@ -14,6 +16,34 @@ public partial class GraphController : Node2D
 		SetupHousesLookup();
 		SetupInitialConnections();
 	}
+
+    public override void _Process(double delta)
+    {
+		// Esse método roda todo frame, é o comportamento do GraphController durante todo o programa
+        base._Process(delta);
+
+		QueueRedraw();
+    }
+
+    public override void _Draw()
+    {
+        base._Draw();
+
+		drawnConnections.Clear();
+		foreach (GreatHouse from in Graph.Keys)
+		{
+			foreach (var to in Graph[from])
+			{
+				Tuple<GreatHouse, GreatHouse> connectionTuple = new Tuple<GreatHouse, GreatHouse>(from, to.Key);
+				
+				// Retorna caso ja tenhamos desenhado a relação casa A casa B, para evitar desenhar casa B com casa A
+				if (drawnConnections.Contains(connectionTuple)) continue; 
+				
+				DrawLine(from.GlobalPosition, to.Key.GlobalPosition, new Color(1, (float)0.84313726, 0, 1), (float)to.Value.Intensity * 5);
+				drawnConnections.Add(connectionTuple);
+			}
+		}
+    }
 
 	private void AddConnection(GreatHouse from, GreatHouse to, double intensity)
 	{
